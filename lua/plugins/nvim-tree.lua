fc1a58eebf6fb32d39f7c10d6400bedaddedc7b5
Nyan <git@nyan.su>
2023-09-29 04:21:19 +0300

    full rewrite

--- lua/plugins/nvim-tree.lua
+++
@@ -1,9 +0,0 @@
-require('nvim-tree').setup {
-    auto_reload_on_write = true,
-    disable_netrw = true,
-    hijack_cursor = true,
-    reload_on_bufenter = true,
-    git = {
-        enable = false,
-    },
-}
