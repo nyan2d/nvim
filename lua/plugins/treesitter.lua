d753ec43d009da0ff7ce5dd521aaf704b4197451
Nyan <git@nyan.su>
2024-05-31 12:19:17 +0300

    treesitter: add parser for markdown & markdown_inline

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
         build = ":TSUpdate",
         config = function()
             require("nvim-treesitter.configs").setup {
-                ensure_installed = { "c", "lua", "rust", "go", "javascript", "python", "html", "css" },
+                ensure_installed = { "c", "lua", "rust", "go", "javascript", "python", "html", "css", "markdown", "markdown_inline" },
                 highlight = { enable = true },
             }
         end,
